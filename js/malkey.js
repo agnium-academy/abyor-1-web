f2854ff2e6f7396e44ab7688c4a2451513d3a9db
Albert Daniel <albertdanieln@yahoo.com>
2016-02-18 23:05:31 +0700

    Repair HomePage, edit malarkey typewriting, about section, history section, and team section

--- js/malkey.js
+++ js/malkey.js
@@ -9,6 +9,6 @@ var typist= malarkey(elem, opts)
 typist
     .type('Science').pause().delete(7)
     .type('Rocket').pause().delete(6)
-    .type('YOU').pause().delete(8)
+    .type('History').pause().delete(12)
     .type('are CloudRocket').pause().delete(15)
     .type('Love ')
