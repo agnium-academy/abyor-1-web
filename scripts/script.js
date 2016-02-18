129acfe720cb6005d94f21ffd360384d777e1b61
sofifitriyah <sofi.fitriyah@basajans.com>
2016-02-18 09:53:18 +0700

    delete 3 folder yang sudah tidak berfungsi, folder images scripts dan styles

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	//$('body').removeClass('jsOff').addClass('jsOn');
-	$("#tabs a").on("click", function(e){
-		$(this.hash).show();
-		//$(this).addClass('current');
-		e.preventDefault();
-	})
-
-});
